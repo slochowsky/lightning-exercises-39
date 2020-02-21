c6167fbd2f998e5a7696fb29555e4b4a1fd5402a
blake slochowsky <blakeslochowsky.com>
2020-02-21 10:53:17 -0600

    practice

---
+++ calculator/main.js
@@ -0,0 +1,51 @@
+// const addNumbers = (first, second) => {
+//     return first + second
+// }
+
+// const sum1 = addNumbers(2, 2)
+// const sum2 = addNumbers(10, 2)
+// const sum3 = addNumbers(20, 2)
+
+// console.log(sum1)
+// console.log(sum2)
+
+// console.log(sum3)
+
+
+// const subtractNumbers = (first, second) => {
+//     return first - second
+// }
+
+// const sum4 = subtractNumbers(49, 50)
+// console.log(sum4)
+
+
+// const person = {
+//     name: "Blake",
+//     age: 25,
+//     hasPets: true,
+//     favoriteFoods: ["Burgers", "Pizza", "Chicken"]
+// }
+
+// person.cohortName = "Cohort 39"
+// console.log(person.cohortName)
+
+// for (const food of person.favoriteFoods) {
+//     if (food === "Burgers") {
+//         console.log(`${food} are the best food in the world!`)
+//     } else {
+//         console.log(food)
+
+//     }
+// }
+
+
+
+
+// const addNumbers = (first, second, third) => {
+//     return first + second + third
+// }
+
+// const sum1 = addNumbers(123, 1234, 124)
+
+// console.log(sum1)
